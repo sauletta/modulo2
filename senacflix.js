b012f06edef0e50e7577814d353bc170fbd7f553
sauletta <saulogalindo@hotmail.com>
2022-02-03 20:03:40 -0300

    Mais arquivos

---
+++ senacflix.js
@@ -0,0 +1,11 @@
+//var Conseguiu= Boolean(false);
+
+//Conseguiu=true;
+
+//if (Conseguiu==true) {setTimeout(function(){
+//    console.log("Primeiro");
+//}, 3000);
+//console.log("Segundo");}
+
+console.log("Usuário 1: assistindo filme");
+console.log("Usuário 2: assistindo série");
