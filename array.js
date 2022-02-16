54efa34cd319de23d895c7d23ebdb556cb4cf8eb
sauletta <saulogalindo@hotmail.com>
2022-02-15 21:13:30 -0300

    Tentativa falha de um array com input

---
+++ array.js
@@ -0,0 +1,33 @@
+const input = require("readline-sync");
+
+// const lista = new Array("pêra", "uva", "maçã", "mamão", "banana");
+// console.log(lista);
+// console.log(lista.length); // métodos são precedidos de ponto (.) para acessar propriedades
+// const tamanhoLista=lista.length;
+// console.log(tamanhoLista)
+
+// const numeros = [9, 2, 5];
+// console.log(numeros[1]);
+
+// const primeiroItem = lista[0];
+// console.log(primeiroItem);
+
+const alunos = new Array("Davidson","Paulo","Sandra","Saulo","Charlinton","Riva");
+console.log(alunos);
+console.log(alunos.length);
+console.log(alunos[3]);
+
+// const alunoNome = input.question("Digite seu nome? ");
+// const alunoIdade = input.question("Digite sua idade? ");
+// const alunoCurso = input.question("Curso de interesse? ");
+
+// const alunoNovo = Array(alunoNome, alunoIdade, alunoCurso);
+
+// console.log("Bem vindo ao curso de " + alunoCurso + ", " + alunoNome + "!");
+
+// function NumeroRandomico(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+// const numeroAleatorio = NumeroRandomico(100, 200);
+// console.log("Sua matrícula é: " + numeroAleatorio + alunoIdade);
